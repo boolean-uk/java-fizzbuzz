d076bbb3d15136ca4eca67abe76b2f9e9e82556a
Ali Keskin <cmdalikeskin@gmail.com>
2023-09-05 00:38:46 +0200

    Finished the assignments

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -1,5 +1,7 @@
 package com.booleanuk.extension;
 
+import java.util.Arrays;
+
 public class Extension {
     public String[] fizzbuzz(int lower, int higher) {
         /* TODO: Implement a function that will create a list of strings containing the numbers from the lower number to the higher number
@@ -7,9 +9,31 @@ public class Extension {
             Where a number is a multiple of five (5, 10, etc) the array should contain the string "Buzz" instead of a number.
             Where a number is a multiple of both five AND three (15, 30, etc) the array should contain the string "Fizzbuzz" instead of a number.
         * */
+        int[] holder = new int[higher - lower + 1];
+        String[] textHOlder = new String[holder.length];
 
 
+        int iCount = 0;
 
-        return null;
+        for (int i = 1; i < holder.length + 1; i++) {
+            if (i % 5 == 0 && i % 3 == 0) {
+                textHOlder[iCount] = "Fizzbuzz";
+                iCount++;
+            } else if (i % 5 == 0) {
+                textHOlder[iCount] = "Buzz";
+                iCount++;
+            } else if (i % 3 == 0) {
+                textHOlder[iCount] = "Fizz";
+                iCount++;
+            } else {
+                textHOlder[iCount] = String.valueOf(i);
+                iCount++;
+            }
+
+
+        }
+        System.out.println(Arrays.toString(textHOlder));
+
+        return textHOlder;
     }
 }
