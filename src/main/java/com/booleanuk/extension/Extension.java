2ca949d7c174edfde308079020577e474fef0fa7
Petros Moustakis <petrosmoukas16@gmail.com>
2023-09-04 14:27:59 +0300

    fizz buzz done

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -7,9 +7,22 @@ public class Extension {
             Where a number is a multiple of five (5, 10, etc) the array should contain the string "Buzz" instead of a number.
             Where a number is a multiple of both five AND three (15, 30, etc) the array should contain the string "Fizzbuzz" instead of a number.
         * */
-
-
-
-        return null;
+        String[] arr = new String[higher - lower + 1];
+        int counter = 0;
+        for (int i = lower; i <= higher ; i++) {
+            if (i == 0) {
+                arr[counter] = "0";
+            } else if (i % 3 == 0 && i % 5 == 0 ) {
+                arr[counter] = "Fizzbuzz";
+            } else if (i % 3 == 0) {
+                arr[counter] = "Fizz";
+            } else if (i % 5 == 0) {
+                arr[counter] = "Buzz";
+            } else {
+                arr[counter] = String.valueOf(i);
+            }
+            counter++;
+        }
+        return arr;
     }
 }
