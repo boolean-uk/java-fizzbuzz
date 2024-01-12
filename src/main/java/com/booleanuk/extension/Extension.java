a00d7d4255d36b6d551cf165d210e5b2d6d7859b
Marit <marit.moe2@ntebb.no>
2024-01-12 09:59:25 +0100

    Solved core and extension FizzBuzz exercise.

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -1,15 +1,22 @@
 package com.booleanuk.extension;
+import com.booleanuk.core.Exercise;
 
 public class Extension {
+
     public String[] fizzbuzz(int lower, int higher) {
         /* TODO: Implement a function that will create a list of strings containing the numbers from the lower number to the higher number
             Where a number is a multiple of three (3, 6, 9, etc) the array should contain the string "Fizz" instead of a number.
             Where a number is a multiple of five (5, 10, etc) the array should contain the string "Buzz" instead of a number.
             Where a number is a multiple of both five AND three (15, 30, etc) the array should contain the string "Fizzbuzz" instead of a number.
         * */
+        String[] nums = new String[higher - lower + 1];
+        for (int i = lower; i <= higher; i++) {
+            nums[i - lower] = "" + i;
+        }
 
+        Exercise exercise = new Exercise();
+        nums = exercise.fizzbuzz(nums);
 
-
-        return null;
+        return nums;
     }
 }
