4c5c8adbfc16d5d31b28972c26d1de93b06d5d87
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-01-12 09:34:47 +0100

    Fixed core and extension

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -1,5 +1,9 @@
 package com.booleanuk.extension;
 
+import com.booleanuk.core.Exercise;
+
+import java.util.ArrayList;
+
 public class Extension {
     public String[] fizzbuzz(int lower, int higher) {
         /* TODO: Implement a function that will create a list of strings containing the numbers from the lower number to the higher number
@@ -8,8 +12,14 @@ public class Extension {
             Where a number is a multiple of both five AND three (15, 30, etc) the array should contain the string "Fizzbuzz" instead of a number.
         * */
 
+        Exercise exercise = new Exercise();
+        int length = higher - lower + 1;
+        String[] numbers = new String[length];
+        for(int i = 0; i < length; i++) {
+            numbers[i] = String.valueOf(lower + i);
+        }
+        exercise.fizzbuzz(numbers);
 
-
-        return null;
+        return numbers;
     }
 }
