4d87cd06397be75861de75a5554f96b065f66ad2
noahlenn <noahlenn@hotmail.com>
2024-01-12 09:49:26 +0100

    Noah Lennestad

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -1,5 +1,7 @@
 package com.booleanuk.core;
 
+import static java.lang.Integer.parseInt;
+
 public class Exercise {
 
     // Since an array can only contain values of a single type, we're providing an array of strings for you to run logic on.
@@ -18,9 +20,17 @@ public class Exercise {
             Where a number is a multiple of five (5, 10, etc) the number in the array should be replaced the string "Buzz".
             Where a number is a multiple of both five AND three (15, 30, etc) the number in the array should be replaced the string "Fizzbuzz".
         * */
+        for(int i = 0; i < nums.length; i++){
+            int num = parseInt(nums[i]);
 
-
-        
+            if((num % 3 == 0) && (num % 5 == 0)){
+                nums[i] = "Fizzbuzz";
+            }else if(num % 3 == 0){
+                nums[i] = "Fizz";
+            }else if(num % 5 == 0){
+                nums[i] = "Buzz";
+            }
+        }
         return nums;
     }
 }
